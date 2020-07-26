e594586a6c652c91ad44f9f5e211fc779eee6450
Tushar Zagade <Tusharbz7276@gmail.com>
2020-07-26 11:53:34 +0530

    added scss material ui theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 import App from './App'
+import '../public/sass/materialize.scss';
 
 
 ReactDom.render(<App />, document.getElementById('root'))
