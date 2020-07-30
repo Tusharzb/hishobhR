e74ea7c717687e3e79861103a4546a8552a19596
Tushar Zagade <Tusharbz7276@gmail.com>
2020-07-30 09:46:01 +0530

    changed baseURL

--- src/services/base.js
+++ src/services/base.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://hishobh.herokuapp.com";
 
 export const getTrackers = (userID) =>{
     return axios.get(`${baseUrl}/api/tracker/${userID}`);
