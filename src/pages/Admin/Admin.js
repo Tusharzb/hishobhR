b78001c0226679634d4cf1b4b286d8100ecd9eda
unknown <Tusharbz7276@gmail.com>
2021-02-26 11:46:20 +0530

    added color picker dropdown in add category

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -4,6 +4,9 @@ import { addCategory, deleteCategory, getCategories } from '../../services/base'
 import { CategoryColumns } from '../../Utils/Utils';
 import './Admin.style.scss';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+
+
 
 const DEFAULT_CLASSNAME = "admin"
 
@@ -12,6 +15,20 @@ const ActionRow = (props) => {
         <button onClick={() => props.deleteCategory()} className="waves-effect waves-light btn red lighten-2">Delete</button>
     )
 }
+
+const ColorOptions = [
+    { label: <div style={{ backgroundColor: "#e88b8b", width: "30px", height: "30px" }}></div>, value: "#e88b8b" },
+    { label: <div style={{ backgroundColor: "#81d69f", width: "30px", height: "30px" }}></div>, value: "#81d69f" },
+    { label: <div style={{ backgroundColor: "#74c3c3", width: "30px", height: "30px" }}></div>, value: "#74c3c3" },
+    { label: <div style={{ backgroundColor: "#6a8ab9", width: "30px", height: "30px" }}></div>, value: "#6a8ab9" },
+    { label: <div style={{ backgroundColor: "#766ab9", width: "30px", height: "30px" }}></div>, value: "#766ab9" },
+    { label: <div style={{ backgroundColor: "#9b6ab9", width: "30px", height: "30px" }}></div>, value: "#9b6ab9" },
+    { label: <div style={{ backgroundColor: "#b96a99", width: "30px", height: "30px" }}></div>, value: "#b96a99" },
+    { label: <div style={{ backgroundColor: "#b9846a", width: "30px", height: "30px" }}></div>, value: "#b9846a" },
+    { label: <div style={{ backgroundColor: "#b9ac6a", width: "30px", height: "30px" }}></div>, value: "#b9ac6a" },
+    { label: <div style={{ backgroundColor: "#adb96a", width: "30px", height: "30px" }}></div>, value: "#adb96a" },
+    { label: <div style={{ backgroundColor: "#8cb96a", width: "30px", height: "30px" }}></div>, value: "#8cb96a" },
+]
 
 
 ActionRow.PropTypes = {
@@ -94,6 +111,7 @@ class Admin extends Component {
             <div className={DEFAULT_CLASSNAME}>
                 <div className={`${DEFAULT_CLASSNAME}-category`}>
                     <input className="inputCategory" placeholder="add category" onChange={(e) => this.handelInput(e.target.value)} value={newCategory} />
+                    <Select options={ColorOptions} className="color-picker" classNamePrefix="picker" required />
                     <button className={`footer waves-effect waves-light btn`} onClick={this.submit}>Add</button>
                     {this.renderTable(showCategory)}
                 </div>
